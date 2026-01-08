3d85968e79894c06bef3f29313c4cda14ea2c2e5
Niche <73772820+niche0905@users.noreply.github.com>
2026-01-08 18:48:16 +0900

    ClientSession 초안 작성
    ClientSession으로 클라이언트에서 사용할 Session class를 디자인 하였다
    우선하여 Network Worker를 만들고 다시 작성해야 할 듯 싶다

---
+++ Source/TimeThief/Network/ClientSession.cpp
@@ -0,0 +1,2 @@
+﻿#include "C:\Github\TimeThief\Intermediate\Build\Win64\x64\TimeThiefEditor\Development\UnrealEd\SharedPCH.UnrealEd.Project.ValApi.ValExpApi.Cpp20.h"
+#include "ClientSession.h"
---
+++ Source/TimeThief/Network/ClientSession.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Sockets.h"
+
+#include "SendBuffer.h"
+
+class FSocket;
+class SendBuffer;
+
+/*-----------------
+   ClientSession
+-----------------*/
+//
+// ClientSession는 클라이언트 측의 세션을 담당하는 클래스입니다.
+//
+
+class TIMETHIEF_API ClientSession : public TSharedFromThis<ClientSession>
+{
+public:
+	ClientSession(class FSocket* Socket);
+	~ClientSession();
+	
+public:
+	void Run();
+	
+	UFUNCTION(BlueprintCallable)
+	void HandleRecvPackets();
+	
+	void SendPacket(TSharedPtr<SendBuffer> SendBuffer);
+	
+	void Disconnect();
+	
+public:
+	FSocket*						Socket;
+	
+	// TODO: Network Worker 먼저 만들기
+	TSharedPtr<class RecvWorker>	RecvWorkerThread;
+	TSharedPtr<class SendWorker>	SendWorkerThread;
+	
+	// SPSC Queue (Single Producer Single Consumer)
+	TQueue<TArray<uint8>>			RecvWorkerQueue;
+	TQueue<TSharedPtr<SendBuffer>>	SendWorkerQueue;
+	
+};
