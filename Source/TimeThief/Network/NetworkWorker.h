c2c55165657ba3a7692fb198c147eea3e36d769f
Niche <73772820+niche0905@users.noreply.github.com>
2026-01-08 18:49:06 +0900

    Network Worker 작성 중
    Network Worker를 작성중에 있지만
    우선하여 Packet Header와 프로토콜 간의 통일을 어떻게 해야할 지 구조적인 고민을 우선적으로 해야할 것 같다

---
+++ Source/TimeThief/Network/NetworkWorker.h
@@ -0,0 +1,76 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Containers/Queue.h"
+#include "Sockets.h"
+
+#include "ClientSession.h"
+#include "SendBuffer.h"
+
+class FSocket;
+class ClientSession;
+class SendBuffer;
+
+/*--------------
+   RecvWorker
+--------------*/
+//
+// RecvWorker는 수신 작업을 처리하는 네트워크 워커 클래스입니다.
+//
+
+class TIMETHIEF_API RecvWorker : public FRunnable
+{
+public:
+	RecvWorker(FSocket* Socket, TSharedPtr<ClientSession> Session);
+	virtual ~RecvWorker();
+	
+	virtual bool Init() override;
+	virtual uint32 Run() override;
+	virtual void Exit() override;
+	
+	void Destroy();
+	
+private:
+	bool ReceivePacket(TArray<uint8>& OutPacket);
+	bool ReceiveDesiredBytes(uint8* Result, int32 Size);
+	
+protected:
+	FRunnableThread* Thread = nullptr;
+	bool Running = true;
+	FSocket* Socket;
+	TWeakPtr<ClientSession> SessionRef;
+
+};
+
+/*--------------
+   SendWorker
+--------------*/
+//
+// SendWorker는 송신 작업을 처리하는 네트워크 워커 클래스입니다.
+//
+
+class TIMETHIEF_API SendWorker : public FRunnable
+{
+public:
+	SendWorker(FSocket* Socket, TSharedPtr<ClientSession> Session);
+	virtual ~SendWorker();
+	
+	virtual bool Init() override;
+	virtual uint32 Run() override;
+	virtual void Exit() override;
+	
+	void Destroy();
+	
+public:
+	bool SendPacket(TSharedPtr<SendBuffer> SendBuffer);
+	
+private:
+	bool SendDesiredBytes(const uint8* Buffer, int32 Size);
+	
+protected:
+	FRunnableThread* Thread = nullptr;
+	bool Running = true;
+	FSocket* Socket;
+	TWeakPtr<ClientSession> SessionRef;
+	
+};
