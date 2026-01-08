cab06c2ffd8fc230e235a863020804ff282d04fd
Niche <73772820+niche0905@users.noreply.github.com>
2026-01-08 17:43:03 +0900

    SendBuffer 작성
    간단하게 송신용 버퍼를 작성하였다

---
+++ Source/TimeThief/Network/SendBuffer.cpp
@@ -0,0 +1,27 @@
+﻿#include "C:\Github\TimeThief\Intermediate\Build\Win64\x64\TimeThiefEditor\Development\UnrealEd\SharedPCH.UnrealEd.Project.ValApi.ValExpApi.Cpp20.h"
+#include "SendBuffer.h"
+
+/*--------------
+   SendBuffer
+--------------*/
+
+SendBuffer::SendBuffer(int32 BufferSize)
+{
+   buffer_.SetNum(BufferSize);
+}
+
+SendBuffer::~SendBuffer()
+{
+}
+
+void SendBuffer::CopyData(void* data, int32 len)
+{
+   // THINK: len을 Capacity()보다 크게 보내는 경우에 대한 예외처리 필요?
+   ::memcpy(buffer_.GetData(), data, len);
+   writeSize_ = len;
+}
+
+void SendBuffer::Close(int32 writeSize)
+{
+   writeSize_ = writeSize;
+}
