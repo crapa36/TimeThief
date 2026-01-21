5e96c7e1123c4062abb05852be84a2af53013bd3
crapa36 <crapa36@gmail.com>
2026-01-21 17:58:37 +0900

    refactor to distribute responsibilities and switch wire rendering to mesh

---
+++ Source/TimeThief/Components/Wire/TimeThiefWirePhysics.cpp
@@ -0,0 +1,137 @@
+﻿#include "Components/Wire/TimeThiefWirePhysics.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/Character.h"
+
+void UTimeThiefWirePhysics::Initialize(UCharacterMovementComponent* InMovementComponent)
+{
+	CachedMovementComponent = InMovementComponent;
+}
+
+void UTimeThiefWirePhysics::ApplyWirePhysics(float DeltaTime, const FVector& AnchorPoint, const FVector& WireStartLocation, float WireLength, const FVector2D& Input)
+{
+	if (!IsValid(CachedMovementComponent)) return;
+
+	const FVector ToAnchor = AnchorPoint - WireStartLocation;
+	const float CurrentDistance = ToAnchor.Size();
+	const FVector WireDirection = ToAnchor.GetSafeNormal();
+
+	
+	if (CurrentDistance > WireLength)
+	{
+		const FVector Velocity = CachedMovementComponent->Velocity;
+		const float RadialSpeed = FVector::DotProduct(Velocity, WireDirection);
+
+		if (RadialSpeed < 0.0f)
+		{
+			CachedMovementComponent->Velocity -= WireDirection * RadialSpeed;
+		}
+
+		const float DistanceError = CurrentDistance - WireLength;
+		if (DistanceError > KINDA_SMALL_NUMBER)
+		{
+			const float CorrectionSpeed = DistanceError * PositionCorrectionSpeed;
+			CachedMovementComponent->Velocity += WireDirection * CorrectionSpeed;
+		}
+	}
+
+	
+	const float HeightDifference = AnchorPoint.Z - WireStartLocation.Z;
+	if (HeightDifference > 0 && HeightDifference < AnchorHeightThreshold)
+	{
+		if (CachedMovementComponent->Velocity.Z > 0)
+		{
+			CachedMovementComponent->Velocity.Z *= VerticalDampingLow;
+		}
+	}
+	else if (HeightDifference <= 0)
+	{
+		if (CachedMovementComponent->Velocity.Z > 0)
+		{
+			CachedMovementComponent->Velocity.Z *= VerticalDampingHigh;
+		}
+	}
+
+	
+	const FVector PullForce = CalculatePullForce(WireDirection, CurrentDistance);
+	const FVector InputForce = CalculateSwingInputForce(WireDirection, Input);
+	FVector TotalForce = PullForce + InputForce;
+
+	
+	const FVector Velocity = CachedMovementComponent->Velocity;
+	const float SpeedSq = Velocity.SizeSquared();
+	const float MaxSpeed = CachedMovementComponent->MaxWalkSpeed * MaxSwingSpeedMultiplier;
+
+	if (SpeedSq > FMath::Square(MaxSpeed))
+	{
+		const FVector DragForce = -Velocity.GetSafeNormal() * (SpeedSq * SwingDragCoefficient);
+		TotalForce += DragForce;
+	}
+
+	CachedMovementComponent->AddForce(TotalForce);
+}
+
+FVector UTimeThiefWirePhysics::CalculatePullForce(const FVector& WireDirection, float CurrentDistance) const
+{
+	if (!IsValid(CachedMovementComponent)) return FVector::ZeroVector;
+
+	const FVector TangentVelocity = GetTangentVelocity(CachedMovementComponent->Velocity, WireDirection);
+	const float TangentSpeedSq = TangentVelocity.SizeSquared();
+
+	const float CentrifugalForceMagnitude = CachedMovementComponent->Mass * CentrifugalMassMultiplier * TangentSpeedSq / FMath::Max(CurrentDistance, MinWireLengthForPhysics);
+
+	float TotalPullMagnitude = CentrifugalForceMagnitude + PullInForce;
+	const float MaxPullForce = PullInForce * 2.0f;
+	TotalPullMagnitude = FMath::Min(TotalPullMagnitude, MaxPullForce);
+
+	return WireDirection * TotalPullMagnitude;
+}
+
+FVector UTimeThiefWirePhysics::CalculateSwingInputForce(const FVector& WireDirection, const FVector2D& Input) const
+{
+	if (!IsValid(CachedMovementComponent) || Input.IsNearlyZero()) return FVector::ZeroVector;
+
+	const ACharacter* Character = Cast<ACharacter>(CachedMovementComponent->GetOwner());
+	if (!Character) return FVector::ZeroVector;
+
+	FVector ViewLoc;
+	FRotator ViewRot;
+	Character->GetActorEyesViewPoint(ViewLoc, ViewRot);
+
+	const FVector ForwardDir = FRotationMatrix(ViewRot).GetUnitAxis(EAxis::X);
+	const FVector RightDir = FRotationMatrix(ViewRot).GetUnitAxis(EAxis::Y);
+	const FVector InputDirection = (ForwardDir * Input.Y + RightDir * Input.X).GetSafeNormal();
+
+	if (InputDirection.IsNearlyZero()) return FVector::ZeroVector;
+
+	const FVector TangentVelocity = GetTangentVelocity(CachedMovementComponent->Velocity, WireDirection);
+	const float TangentSpeedSq = TangentVelocity.SizeSquared();
+
+	FVector ForceDirection = FVector::ZeroVector;
+
+	if (TangentSpeedSq > FMath::Square(100.0f))
+	{
+		const FVector SwingDir = TangentVelocity.GetSafeNormal();
+		const float Projection = FVector::DotProduct(InputDirection, SwingDir);
+		ForceDirection = SwingDir * Projection;
+	}
+	else
+	{
+		const float InputAlongWire = FVector::DotProduct(InputDirection, WireDirection);
+		const FVector RadialInputComponent = WireDirection * InputAlongWire;
+		const FVector RawTangentInput = InputDirection - RadialInputComponent;
+
+		if (RawTangentInput.SizeSquared() > KINDA_SMALL_NUMBER)
+		{
+			ForceDirection = RawTangentInput.GetSafeNormal();
+		}
+	}
+
+	return ForceDirection * SwingInputForce;
+}
+
+FVector UTimeThiefWirePhysics::GetTangentVelocity(const FVector& Velocity, const FVector& WireDirection) const
+{
+	const float VelocityAlongWire = FVector::DotProduct(Velocity, WireDirection);
+	return Velocity - WireDirection * VelocityAlongWire;
+}
---
+++ Source/TimeThief/Components/Wire/TimeThiefWireTargeting.cpp
@@ -0,0 +1,103 @@
+﻿#include "Components/Wire/TimeThiefWireTargeting.h"
+#include "GameFramework/Character.h"
+#include "Engine/World.h"
+
+UTimeThiefWireTargeting::UTimeThiefWireTargeting()
+{
+	CollisionObjectTypes.Add(UEngineTypes::ConvertToObjectType(ECC_WorldStatic));
+	CollisionObjectTypes.Add(UEngineTypes::ConvertToObjectType(ECC_WorldDynamic));
+}
+
+void UTimeThiefWireTargeting::Initialize(ACharacter* InCharacter)
+{
+	CachedCharacter = InCharacter;
+}
+
+bool UTimeThiefWireTargeting::FindBestAnchorTarget(FVector& OutTargetLocation, const FVector& StartLocation, const FVector& AimDirection, float MaxLength)
+{
+	if (!IsValid(CachedCharacter)) return false;
+
+	UWorld* World = CachedCharacter->GetWorld();
+	if (!World) return false;
+
+	const FVector EndLocation = StartLocation + AimDirection * MaxLength;
+
+	FCollisionQueryParams QueryParams;
+	QueryParams.AddIgnoredActor(CachedCharacter);
+
+	TArray<FHitResult> HitResults;
+	bool bFoundAny = World->SweepMultiByObjectType(
+		HitResults, StartLocation, EndLocation, FQuat::Identity,
+		FCollisionObjectQueryParams(CollisionObjectTypes),
+		FCollisionShape::MakeSphere(AutoAimRadius),
+		QueryParams
+	);
+
+	if (!bFoundAny) return false;
+
+	float BestScore = -FLT_MAX;
+	FVector BestLocation = FVector::ZeroVector;
+	bool bFoundCandidate = false;
+
+	for (const FHitResult& Hit : HitResults)
+	{
+		float DistanceToPlayer = FVector::Dist(StartLocation, Hit.ImpactPoint);
+		if (DistanceToPlayer < MinTargetDistance) continue;
+
+		if (!bAllowFloorAttachment && Hit.ImpactNormal.Z >= 0.7f) continue;
+
+		bool bIsLedge = false;
+		const FVector LedgeCheckStart = Hit.ImpactPoint + FVector(0, 0, LedgeCheckHeight);
+		const FVector LedgeCheckEnd = LedgeCheckStart + (Hit.ImpactNormal * -20.0f);
+		
+		FHitResult LedgeHit;
+		bool bLedgeBlocked = World->LineTraceSingleByObjectType(
+			LedgeHit, LedgeCheckStart, LedgeCheckEnd,
+			FCollisionObjectQueryParams(CollisionObjectTypes),
+			QueryParams
+		);
+
+		if (!bLedgeBlocked)
+		{
+			bIsLedge = true;
+		}
+
+		float Score = -FMath::PointDistToLine(Hit.ImpactPoint, AimDirection, StartLocation) * AimAccuracyWeight;
+		
+		if (bIsLedge)
+		{
+			Score += 1000.0f;
+		}
+
+		if (Score > BestScore)
+		{
+			BestScore = Score;
+			BestLocation = bIsLedge ? Hit.ImpactPoint + FVector(0, 0, 5.0f) : FVector(Hit.ImpactPoint);
+			bFoundCandidate = true;
+		}
+	}
+
+	if (bFoundCandidate)
+	{
+		OutTargetLocation = BestLocation;
+		return true;
+	}
+
+	return false;
+}
+
+bool UTimeThiefWireTargeting::CheckAnchorCollision(const FVector& Start, const FVector& End, FHitResult& OutHit, AActor* IgnoredActor)
+{
+	if (!IsValid(CachedCharacter)) return false;
+	UWorld* World = CachedCharacter->GetWorld();
+	if (!World) return false;
+
+	FCollisionQueryParams QueryParams;
+	QueryParams.AddIgnoredActor(IgnoredActor);
+
+	return World->LineTraceSingleByObjectType(
+		OutHit, Start, End,
+		FCollisionObjectQueryParams(CollisionObjectTypes),
+		QueryParams
+	);
+}
