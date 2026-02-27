35fedb95c86c5472989594f5aa8045dd5e4a763a
kimhyeongil <alfkcjstk853@tukorea.ac.kr>
2026-02-27 15:34:32 +0900

    Feat: Implement minimap player and storm zone UI
    - Map coordinates and storm zone are hardcoded for algorithm test.
    - GameState integration will be implemented later.

---
+++ Source/TimeThief/UI/Minimap/MinimapWidget.cpp
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MinimapWidget.h"
+
+#include "Components/CanvasPanelSlot.h"
+#include "Components/Image.h"
+
+void UMinimapWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	
+	Player = GetOwningPlayerPawn();
+	MinimapSize = Cast<UCanvasPanelSlot>(Minimap_Image->Slot)->GetSize();
+	
+	StormZoneDMI = StormZone_Image->GetDynamicMaterial();
+}
+
+void UMinimapWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+	
+	if (IsVisible() && IsValid(Player))
+	{
+		const FVector PlayerLocation = Player->GetActorLocation();
+		FVector2D MinimapPosition;
+		
+		MinimapPosition.X = PlayerLocation.Y / MapSize.Y * MinimapSize.X;
+		MinimapPosition.Y = PlayerLocation.X / MapSize.X * -MinimapSize.Y;
+		
+		Player_Icon->SetRenderTranslation(MinimapPosition);
+	}
+	ElapsedTime += InDeltaTime;
+	if (StormZoneCenter.Equals(DestStormZoneCenter))
+	{
+		
+	}
+	else
+	{
+		float AlphaRatio = FMath::Min(ElapsedTime / 10, 1);
+		StormZoneCenter = FMath::Lerp(CurrentStormZoneCenter, DestStormZoneCenter, AlphaRatio);
+		StormZoneRadius = FMath::Lerp(CurrentStormZoneRadius, DestStormZoneRadius, AlphaRatio);
+		StormZoneDMI->SetVectorParameterValue(FName{"CenterPosition"}, FVector{StormZoneCenter.X, StormZoneCenter.Y, 0});
+		StormZoneDMI->SetScalarParameterValue(FName{"Radius"}, StormZoneRadius);
+	}
+}
---
+++ Source/TimeThief/UI/Minimap/MinimapWidget.h
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MinimapWidget.generated.h"
+
+/**
+ * 
+ */
+
+class UImage;
+class UMaterialInstanceDynamic;
+
+UCLASS()
+class TIMETHIEF_API UMinimapWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void NativeConstruct() override;
+	
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+	
+public:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> Minimap_Image;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> Player_Icon;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> StormZone_Image;
+	
+	UPROPERTY()
+	TObjectPtr<UMaterialInstanceDynamic> StormZoneDMI;
+	
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<AActor> Player;
+	
+	UPROPERTY(EditAnywhere)
+	FVector2D MapSize;
+	
+	FVector2D MinimapSize;
+	
+	float ElapsedTime{0};
+	FVector2D CurrentStormZoneCenter{0.5f};
+	float CurrentStormZoneRadius{0.5f};
+	
+	FVector2D StormZoneCenter{0.5f};
+	float StormZoneRadius{0.5f};
+	
+	FVector2D DestStormZoneCenter{0.7f, 0.7f};
+	float DestStormZoneRadius{0.3f};
+};
