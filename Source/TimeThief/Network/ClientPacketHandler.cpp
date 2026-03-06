3cbeffff8c1bacd043822985f6f1730d98ec3eff
niche0905 <yudle537@gmail.com>
2026-03-06 21:32:31 +0900

    Protocol 업데이트 적용
    Protocol 업데이트로 인한 PacketHandler 코드에 대한 변화를 적용해 빌드를 진행했다

--- Source/TimeThief/Network/ClientPacketHandler.cpp
+++ Source/TimeThief/Network/ClientPacketHandler.cpp
@@ -1,7 +1,7 @@
 #include "Generated/ClientPacketHandler.h"
 #include "PacketSession.h"
 
-PacketHandlerFunc GPacketHandler[UINT16_MAX];
+PacketHandlerFunc GPacketHandler[kMaxMessageId + 1];
 
 bool Handle_INVALID(PacketSessionRef& session, BYTE* buffer, int32 len)
 {
@@ -9,66 +9,66 @@ bool Handle_INVALID(PacketSessionRef& session, BYTE* buffer, int32 len)
 }
 
 
-bool Handle_S_HandshakeRes(PacketSessionRef& session, BYTE* buffer, int32 len)
+bool Handle_S_HandshakeRes(PacketSessionRef& session, const se::auth::S_HandshakeRes& pkt)
 {
 	return false;
 }
-bool Handle_S_LoginRes(PacketSessionRef& session, BYTE* buffer, int32 len)
-{
-	return false;
-}
-
-bool Handle_S_Pong(PacketSessionRef& session, BYTE* buffer, int32 len)
+bool Handle_S_LoginRes(PacketSessionRef& session, const se::auth::S_LoginRes& pkt)
 {
 	return false;
 }
 
-bool Handle_S_LobbyEnterRes(PacketSessionRef& session, BYTE* buffer, int32 len)
+bool Handle_S_Pong(PacketSessionRef& session, const se::auth::S_Pong& pkt)
 {
 	return false;
 }
 
-bool Handle_S_MatchQueueEnterRes(PacketSessionRef& session, BYTE* buffer, int32 len)
+bool Handle_S_LobbyEnterRes(PacketSessionRef& session, const se::lobby::S_LobbyEnterRes& pkt)
 {
 	return false;
 }
 
-bool Handle_S_MatchQueueCancelRes(PacketSessionRef& session, BYTE* buffer, int32 len)
+bool Handle_S_MatchQueueEnterRes(PacketSessionRef& session, const se::lobby::S_MatchQueueEnterRes& pkt)
 {
 	return false;
 }
 
-bool Handle_N_MatchFound(PacketSessionRef& session, BYTE* buffer, int32 len)
+bool Handle_S_MatchQueueCancelRes(PacketSessionRef& session, const se::lobby::S_MatchQueueCancelRes& pkt)
 {
 	return false;
 }
 
-bool Handle_N_RoomReadyChanged(PacketSessionRef& session, BYTE* buffer, int32 len)
+bool Handle_N_MatchFound(PacketSessionRef& session, const se::lobby::N_MatchFound& pkt)
 {
 	return false;
 }
 
-bool Handle_N_GameStart(PacketSessionRef& session, BYTE* buffer, int32 len)
+bool Handle_N_RoomReadyChanged(PacketSessionRef& session, const se::room::N_RoomReadyChanged& pkt)
 {
 	return false;
 }
 
-bool Handle_S_EntityState(PacketSessionRef& session, BYTE* buffer, int32 len)
+bool Handle_N_GameStart(PacketSessionRef& session, const se::room::N_GameStart& pkt)
 {
 	return false;
 }
 
-bool Handle_N_EntitySpawn(PacketSessionRef& session, BYTE* buffer, int32 len)
+bool Handle_S_EntityState(PacketSessionRef& session, const se::room::S_EntityState& pkt)
 {
 	return false;
 }
 
-bool Handle_N_EntityDespawn(PacketSessionRef& session, BYTE* buffer, int32 len)
+bool Handle_N_EntitySpawn(PacketSessionRef& session, const se::room::N_EntitySpawn& pkt)
 {
 	return false;
 }
 
-bool Handle_N_HitEvent(PacketSessionRef& session, BYTE* buffer, int32 len)
+bool Handle_N_EntityDespawn(PacketSessionRef& session, const se::room::N_EntityDespawn& pkt)
+{
+	return false;
+}
+
+bool Handle_N_HitEvent(PacketSessionRef& session, const se::room::N_HitEvent& pkt)
 {
 	return false;
 }
