daa050b60ca5561f4168b305917e483ebfcd28e6
niche0905 <yudle537@gmail.com>
2026-03-19 16:54:20 +0900

    NetwrokEntityComponent 수정
    헬퍼 함수를 몇개 추가하고 부재하였던 스페셜 함수 정의를 추가하였다

--- Source/TimeThief/Network/NetworkEntityComponent.cpp
+++ Source/TimeThief/Network/NetworkEntityComponent.cpp
@@ -3,3 +3,23 @@
 
 #include "NetworkEntityComponent.h"
 
+UNetworkEntityComponent::UNetworkEntityComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+bool UNetworkEntityComponent::CanSendInput() const
+{
+	return ControlType == ENetworkControlType::Local;
+}
+
+bool UNetworkEntityComponent::ShouldApplyNetworkState() const
+{
+	return ControlType != ENetworkControlType::Local;
+}
+
+void UNetworkEntityComponent::ResetNetworkEntity()
+{
+	EntityId = 0;
+	ControlType = ENetworkControlType::None;
+}
--- Source/TimeThief/Network/NetworkEntityComponent.h
+++ Source/TimeThief/Network/NetworkEntityComponent.h
@@ -14,18 +14,24 @@ class TIMETHIEF_API UNetworkEntityComponent : public UActorComponent
 	GENERATED_BODY()
 
 public:
+	UNetworkEntityComponent();
+	
 	uint32 GetEntityId() const { return EntityId; }
 	void SetEntityId(uint32 InEntityId) { EntityId = InEntityId; }
 
 	ENetworkControlType GetControlType() const { return ControlType; }
 	void SetControlType(ENetworkControlType InControlType) { ControlType = InControlType; }
 	
+	bool IsValidEntity() const { return EntityId != 0; }
+	
 	bool IsLocalControlled() const { return ControlType == ENetworkControlType::Local; }
 	bool IsRemoteControlled() const { return ControlType == ENetworkControlType::Remote; }
 	bool IsServerAuthoritative() const { return ControlType == ENetworkControlType::ServerAuth; }
 	
-	bool CanSendInput() const { return ControlType == ENetworkControlType::Local; }
-	bool ShouldApplyNetworkState() const { return ControlType != ENetworkControlType::Local; }
+	bool CanSendInput() const;
+	bool ShouldApplyNetworkState() const;
+	
+	void ResetNetworkEntity();
 	
 private:
 	UPROPERTY()
