9348af9d7c841927c23d0cfa185bad8d83a434cc
crapa36 <crapa36@gmail.com>
2026-03-31 00:02:44 +0900

    Handle networked aim and pawn initialization

--- Source/TimeThief/Animation/Player/TimeThiefPlayerAnimInstance.cpp
+++ Source/TimeThief/Animation/Player/TimeThiefPlayerAnimInstance.cpp
@@ -181,12 +181,13 @@ void UTimeThiefPlayerAnimInstance::UpdateSpreadAndRecoil(float DeltaSeconds) {
 
 void UTimeThiefPlayerAnimInstance::UpdateAimDirection() {
 	if (!PlayerCharacter) {
-		AimPitch = 0.0f;
-		AimDirection = FVector::ForwardVector;
-		WorldAimLocation = FVector::ZeroVector;
 		return;
 	}
-
+	if (!PlayerCharacter->IsLocallyControlled()) {
+		AimPitch = PlayerCharacter->GetNetworkPitch();
+		AimDirection = FRotator(AimPitch, PlayerCharacter->GetNetworkYaw(), 0.0f).Vector();
+		return;
+	}
 	FVector CameraLocation = FVector::ZeroVector;
 	FRotator CameraRotation = PlayerCharacter->GetControlRotation();
 
