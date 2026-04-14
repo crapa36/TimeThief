60c31f54c2a2b13c8536277488ded2c44acae841
niche0905 <yudle537@gmail.com>
2026-04-14 16:59:25 +0900

    Log 삭제
    에디터에 출력하는 Log를 삭제하였다

--- Source/TimeThief/Network/NetworkGameInstanceSubsystem.cpp
+++ Source/TimeThief/Network/NetworkGameInstanceSubsystem.cpp
@@ -1108,7 +1108,7 @@ void UNetworkGameInstanceSubsystem::HandleTimeStormChange(const se::game::N_Time
 {
 	check(IsInGameThread());
 	
-	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, FString::Printf(TEXT("Time Storm Changed: Radus=%.1f, Wait=%.1f, Shrink=%.1f"), Pkt.radius(), Pkt.waiting_time(), Pkt.shrinking_time()));
+	// GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, FString::Printf(TEXT("Time Storm Changed: Radus=%.1f, Wait=%.1f, Shrink=%.1f"), Pkt.radius(), Pkt.waiting_time(), Pkt.shrinking_time()));
 	
 	if (!IsRoomPlayableState(PlayState))
 	{
