e90603300bdc2f802b04a5316e16ee33309b5ab3
niche0905 <yudle537@gmail.com>
2026-05-21 23:00:08 +0900

    Offset 추출
    간단하게 Log로 Offset을 추출함

---
+++ Source/TimeThief/Monster/Offset/AN_CatOffset.h
@@ -0,0 +1,29 @@
+﻿
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AN_CatOffset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TIMETHIEF_API UAN_CatOffset : public UAnimNotify
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName SocketName = TEXT("FX_Cannon_Socket");
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector LocationOffset = FVector::ZeroVector;
+
+	virtual void Notify(
+		USkeletalMeshComponent* MeshComp,
+		UAnimSequenceBase* Animation
+	) override;
+	
+};
