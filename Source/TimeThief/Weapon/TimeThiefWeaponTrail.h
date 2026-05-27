e97f9b49a670c76e3ed13a8673f9c21abb6a615b
crapa36 <crapa36@gmail.com>
2026-05-27 17:11:05 +0900

    Compute hitscan trail lifetime, add fade params

--- Source/TimeThief/Weapon/TimeThiefWeaponTrail.h
+++ Source/TimeThief/Weapon/TimeThiefWeaponTrail.h
@@ -20,15 +20,24 @@ enum class ETimeThiefWeaponTrailType : uint8
 struct FTimeThiefWeaponTrailStyle
 {
 	FTimeThiefWeaponTrailStyle() = default;
-	FTimeThiefWeaponTrailStyle(float InWidth, float InLifetime, const FLinearColor& InColor = FLinearColor::White)
+	FTimeThiefWeaponTrailStyle(
+		float InWidth,
+		float InLifetime,
+		float InFadeSpeed = 8000.0f,
+		float InFadeDistance = 800.0f,
+		const FLinearColor& InColor = FLinearColor::White)
 		: Width(InWidth)
 		, Lifetime(InLifetime)
+		, FadeSpeed(InFadeSpeed)
+		, FadeDistance(InFadeDistance)
 		, Color(InColor)
 	{
 	}
 
 	float Width = 0.0f;
 	float Lifetime = 0.0f;
+	float FadeSpeed = 8000.0f;
+	float FadeDistance = 800.0f;
 	FLinearColor Color = FLinearColor::White;
 };
 
@@ -56,12 +65,15 @@ private:
 	inline static const FName TrailColorParameterName{TEXT("User.TrailColor")};
 	inline static const FName TrailWidthParameterName{TEXT("User.TrailWidth")};
 	inline static const FName TrailLifetimeParameterName{TEXT("User.TrailLifetime")};
+	inline static const FName TrailFadeSpeedParameterName{TEXT("User.TrailFadeSpeed")};
+	inline static const FName TrailFadeDistanceParameterName{TEXT("User.TrailFadeDistance")};
 
-	inline static const FTimeThiefWeaponTrailStyle RifleTrail{5.0f, 1.0f};
-	inline static const FTimeThiefWeaponTrailStyle ShotgunPelletTrail{5.0f, 1.0f};
+	inline static const FTimeThiefWeaponTrailStyle RifleTrail{5.0f, 0.15f, 8000.0f, 800.0f};
+	inline static const FTimeThiefWeaponTrailStyle ShotgunPelletTrail{5.0f, 0.15f, 8000.0f, 800.0f};
 	inline static const FTimeThiefWeaponTrailStyle RocketTrail{5.0f, 2.0f};
 	inline static const FTimeThiefWeaponTrailStyle GrenadeTrail{3.0f, 2.0f};
 
 	const FTimeThiefWeaponTrailStyle& GetTrailStyle(ETimeThiefWeaponTrailType TrailType) const;
-	void ApplyTrailParameters(UNiagaraComponent& TrailComponent, const FVector& Start, const FVector& End, const FTimeThiefWeaponTrailStyle& Style) const;
+	float GetHitscanLifetime(const FVector& Start, const FVector& End, const FTimeThiefWeaponTrailStyle& Style) const;
+	void ApplyTrailParameters(UNiagaraComponent& TrailComponent, const FVector& Start, const FVector& End, const FTimeThiefWeaponTrailStyle& Style, float Lifetime) const;
 };
